030a7a655007c8c3d8c3ab46f9bfaee902521d9f
Nisarg Patel <53119205+Atheist-Guy18@users.noreply.github.com>
2021-07-31 18:54:08 +0530

    Update and rename hailstone nums.py to Collatz Conjecture.py

--- Collatz Conjecture.py
+++ Collatz Conjecture.py
@@ -1,19 +1,25 @@
 import matplotlib.pyplot as plt
 import networkx as nx
+from tabulate import tabulate
 
 class Hailstone():
   frequency= {}
   g = nx.DiGraph()
+  seeder = []
+  
   def __init__(self,num):
     Hailstone.g.add_node(num)
     hsnums = Hailstone.hailstonenums(num)
+    Hailstone.seeder.append((num,hsnums))
     keys = set(hsnums)
     for key in keys:
       if key in Hailstone.frequency:
         Hailstone.frequency[key] += hsnums.count(key)
       else:
         Hailstone.frequency[key] = hsnums.count(key)
+  
   def hailstonenums(seed):
+    '''The theory logic'''
     hsnums = []
     prev = seed
     while seed != 1:
@@ -25,13 +31,30 @@ class Hailstone():
       Hailstone.g.add_node(seed)
       Hailstone.g.add_edge(prev,seed)
     return(hsnums)
+  
   def bardisplay():
+    '''A Bar distribution of Hailstone numbers'''
     keys = Hailstone.frequency.keys()
     values = Hailstone.frequency.values()
     plt.bar(keys, values)
+  
   def tree():
+    '''
+    Network structure of the relationship.
+    P.S. Only compatible with small range of inputs.
+    '''
     x = nx.DiGraph(set(Hailstone.g.edges()))
     nx.draw(x,with_labels=1)
+  
+  def data():
+    '''Table display of the data'''
+    mydata = Hailstone.seeder
+    header = ["Number", "Hailstone List"]
+    print(tabulate(mydata, headers=header, tablefmt="grid"))
+  
+#Input numbers
 for i in range(1,15):
   x = Hailstone(i)
+#Method Calls
 Hailstone.bardisplay()
+Hailstone.data()
